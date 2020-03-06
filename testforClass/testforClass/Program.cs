82a36d11e5c83959983d9da1781150a0071f9588
lpdink <906618000@qq.com>
2020-03-06 11:13:56 +0800

    3-6

---
+++ testforClass/testforClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace testforClass
+{
+    public class BaseClass
+    {
+        private int a;
+        public int A { get { return a; } }
+        public BaseClass(int a)
+        {
+            this.a = a;
+        }
+    }
+    class kid : BaseClass
+    {
+        private int t = BaseClass.A;
+        kid() : base(BaseClass.A) { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
