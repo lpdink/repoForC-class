e5f148eaa48ca36fafc5b6af8eb5230fe24fdeda
lpdink <906618000@qq.com>
2020-03-18 23:41:52 +0800

    提交3月13日，闹钟事件待修缮

---
+++ 3-13/Console-clock/Console-clock/Program.cs
@@ -0,0 +1,61 @@
+﻿/*使用事件机制，模拟实现一个闹钟功能。
+ * 闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息。*/
+
+using System;
+
+namespace Console_clock
+{
+    public delegate void clockHandler(object sender, ClockEventArgs args);
+    public class ClockEventArgs { }
+    public class Clock
+    {
+        public int H { set; get; }
+        public int M { set; get; }
+        public int S { set; get; }
+        //走时
+        public event clockHandler TimeUp;
+        //响铃
+        //public event clockHandler ClockRing;
+        //显示时间
+        public void displayTime()
+        {
+            ClockEventArgs args = new ClockEventArgs();
+            Console.WriteLine(this.H);
+            Console.WriteLine(this.M);
+            Console.WriteLine(this.S);
+            TimeUp(this, args);
+        }
+        //设置时间
+        public void setTime(int h, int m, int s)
+        {
+
+            H = h;
+            M = m;
+            S = s;
+            
+        }
+       
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello!");
+            Clock clock1 = new Clock();
+            void getTime(object sender,ClockEventArgs args)
+            {
+                Console.WriteLine("getTime");
+            }
+            clock1.setTime(10,10,10);
+            clock1.TimeUp += getTime;
+            clock1.displayTime();
+        }
+    }
+        
+            
+} 
+
+
+    
+
---
+++ 3-13/Console-list/Console-list/Program.cs
@@ -0,0 +1,113 @@
+﻿/*为示例中的泛型链表类添加类似于List<T>类的
+ * ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，
+ * 求最大值、最小值和求和（使用lambda表达式实现）*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        //action是一个需要传入一个泛型参数的方法.
+        public  void ForEach(Action<T> action)
+        {
+            for(Node<T> x = head; x != null; x = x.Next)
+            {
+                action(x.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            void display()
+            {
+
+            }*/
+            
+
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            /*
+            for (Node<int> node = intlist.Head;
+                  node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }*/
+            intlist.ForEach((x)=>Console.WriteLine(x));
+            int maxnum = 0, minnum = 0, sunnum = 0;
+            intlist.ForEach((x) => { if(x > maxnum )  maxnum = x; });
+            intlist.ForEach((x) => { if (x < minnum) minnum = x; });
+            intlist.ForEach((x) => { sunnum += x; });
+            Console.WriteLine("max  " + maxnum+"  min  "+ minnum+"  sum:  "+sunnum);
+
+
+            GenericList<string> strList = new GenericList<string>();
+            for (int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for (Node<string> node = strList.Head;
+                    node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+
+
+        }
+
+    }
+}
